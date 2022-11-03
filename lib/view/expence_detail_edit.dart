684e3ed5b6969ede70c37e01912fb589910e6880
Ayaka Arai <spr2140038@stu.o-hara.ac.jp>
2022-11-03 14:42:16 +0900

    edit直した

--- lib/view/expence_detail_edit.dart
+++ lib/view/expence_detail_edit.dart
@@ -16,18 +16,18 @@ class ExpenseDetailEdit extends StatefulWidget {
 
 class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
   late int expense_id;
-  late String expense_category_code;
+  //late String expense_category_code;
   late int expense_total_money;
   late int expense_consuption_tax;
   late int expense_amount_including_tax;
   late DateTime expense_datetime;
   late String expense_memo;
-  late DateTime createdAt;
+  late DateTime created_at;
   //final List<String> _list = <String>['男の子', '女の子', '不明']; // 性別のDropdownの項目を設定
   //late String _selected; // Dropdownの選択値を格納するエリア
   //String value = '不明'; // Dropdownの初期値
-  //static const int textExpandedFlex = 1; // 見出しのexpaded flexの比率
-  //static const int dataExpandedFlex = 4; // 項目のexpanede flexの比率
+  static const int textExpandedFlex = 1; // 見出しのexpaded flexの比率
+  static const int dataExpandedFlex = 4; // 項目のexpanede flexの比率
 
 // Stateのサブクラスを作成し、initStateをオーバーライドすると、wedgit作成時に処理を動かすことができる。
 // ここでは、各項目の初期値を設定する
@@ -35,28 +35,29 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
   void initState() {
     super.initState();
     expense_id = widget.expenses?.expense_id ?? 0;
-    expense_category_code = widget.expenses?.expense_category_code ?? '未設定';
-    expense_total_money = widget.cats?.birthday ?? '';
-    gender = widget.cats?.gender ?? '';
-    _selected = widget.cats?.gender ?? '不明';
-    memo = widget.cats?.memo ?? '';
-    createdAt = widget.cats?.createdAt ?? DateTime.now();
+    //expense_category_code = widget.expenses?.expense_category_code ?? '未設定';
+    expense_total_money = widget.expenses?.expense_total_money ?? 0;
+    expense_consuption_tax = widget.expenses?.expense_consuption_tax ?? 0;
+    expense_amount_including_tax = widget.expenses?.expense_amount_including_tax ?? 0;
+    expense_datetime = widget.expenses?.expense_datetime ?? DateTime.now();
+    expense_memo = widget.expenses?.expense_memo ?? '';
+    created_at = widget.expenses?.created_at ?? DateTime.now();
   }
 
 // Dropdownの値の変更を行う
-  void _onChanged(String? value) {
-    setState(() {
-      _selected = value!;
-      gender = _selected;
-    });
-  }
+  //void _onChanged(String? value) {
+    //setState(() {
+      //_selected = value!;
+      //gender = _selected;
+    //});
+  //}
 
 // 詳細編集画面を表示する
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('猫編集'),
+        title: const Text('支出編集'),
         actions: [
           buildSaveButton(), // 保存ボタンを表示する
         ],
@@ -67,7 +68,7 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
             // 名前の行の設定
             const Expanded(                   // 見出し（名前）
               flex: textExpandedFlex,
-              child: Text('名前',
+              child: Text('合計金額',
                 textAlign: TextAlign.center,
               ),
             ),
@@ -75,14 +76,14 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
               flex: dataExpandedFlex,
               child: TextFormField(
                 maxLines: 1,
-                initialValue: name,
-                decoration: const InputDecoration(
-                  hintText: '名前を入力してください',
-                ),
-                validator: (name) => name != null && name.isEmpty
-                    ? '名前は必ず入れてね'
-                    : null, // validateを設定
-                onChanged: (name) => setState(() => this.name = name),
+                initialValue: expense_total_money ,
+                //decoration: const InputDecoration(
+                  //hintText: '名前を入力してください',
+                //),
+                //validator: (name) => name != null && name.isEmpty
+                    //? '名前は必ず入れてね'
+                    //: null, // validateを設定
+                onChanged: (expense_total_money) => setState(() => this.expense_total_money = expense_total_money),
               ),
             ),
           ]),
@@ -90,28 +91,23 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
           Row(children: [
             const Expanded(                     // 見出し（性別）
               flex: textExpandedFlex,
-              child: Text('性別',
+              child: Text('消費税',
                 textAlign: TextAlign.center,
               ),
             ),
             Expanded(                           // 性別をドロップダウンで設定
               flex: dataExpandedFlex,
-              child: DropdownButton(
-                items: _list.map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                value: _selected,
-                onChanged: _onChanged,
+              child: TextFormField(
+                maxLines: 1,
+                initialValue: expense_consuption_tax,
+                onChanged: (expense_amount_including_tax) => setState(() => this.expense_consuption_tax = expense_consuption_tax),
               ),
             ),
           ]),
           Row(children: [
             const Expanded(                 // 見出し（誕生日）
               flex: textExpandedFlex,
-              child: Text('誕生日',
+              child: Text('税込金額',
                 textAlign: TextAlign.center,
               ),
             ),
@@ -119,12 +115,31 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
               flex: dataExpandedFlex,
               child: TextFormField(
                 maxLines: 1,
-                initialValue: birthday,
-                decoration: const InputDecoration(
-                  hintText: '誕生日を入力してください',
-                ),
-                onChanged: (birthday) =>
-                    setState(() => this.birthday = birthday),
+                initialValue: expense_amount_including_tax,
+                //decoration: const InputDecoration(
+                  //hintText: '誕生日を入力してください',
+                //),
+                onChanged: (expense_amount_including_tax) =>
+                    setState(() => this.expense_amount_including_tax = expense_amount_including_tax),
+              ),
+            ),
+          ]),
+          Row(children: [
+            const Expanded(                     // 見出し（メモ）
+                flex: textExpandedFlex,
+                child: Text('日付',
+                  textAlign: TextAlign.center,
+                )
+            ),
+            Expanded(                           // メモ入力エリアの設定
+              flex: dataExpandedFlex,
+              child: TextFormField(
+                maxLines: 1,
+                initialValue: expense_datetime,
+                //decoration: const InputDecoration(
+                  //hintText: 'メモを入力してください',
+                //),
+                onChanged: (expense_datetime) => setState(() => this.expense_datetime = expense_datetime),
               ),
             ),
           ]),
@@ -139,11 +154,11 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
               flex: dataExpandedFlex,
               child: TextFormField(
                 maxLines: 1,
-                initialValue: memo,
-                decoration: const InputDecoration(
-                  hintText: 'メモを入力してください',
-                ),
-                onChanged: (memo) => setState(() => this.memo = memo),
+                initialValue: expense_memo,
+                //decoration: const InputDecoration(
+                //hintText: 'メモを入力してください',
+                //),
+                onChanged: (expense_memo) => setState(() => this.expense_memo = expense_memo),
               ),
             ),
           ]),
@@ -154,7 +169,7 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
 
 // 保存ボタンの設定
   Widget buildSaveButton() {
-    final isFormValid = name.isNotEmpty;
+    //final isFormValid = name.isNotEmpty;
 
     return Padding(
       padding: const EdgeInsets.all(10.0),
@@ -162,47 +177,52 @@ class _ExpenseDetailEditState extends State<ExpenseDetailEdit> {
         child: const Text('保存'),
         style: ElevatedButton.styleFrom(
           onPrimary: Colors.white,
-          primary: isFormValid ? Colors.redAccent : Colors.grey.shade700,
+          //primary: isFormValid ? Colors.redAccent : Colors.grey.shade700,
         ),
-        onPressed: createOrUpdateCat, // 保存ボタンを押したら実行する処理を指定する
+        onPressed: createOrUpdateExpense, // 保存ボタンを押したら実行する処理を指定する
       ),
     );
   }
 
 // 保存ボタンを押したとき実行する処理
-  void createOrUpdateCat() async {
-    final isUpdate = (widget.cats != null);     // 画面が空でなかったら
+  void createOrUpdateExpense() async {
+    final isUpdate = (widget.expenses != null);     // 画面が空でなかったら
 
     if (isUpdate) {
-      await updateCat();                        // updateの処理
+      await updateExpense();                        // updateの処理
     } else {
-      await createCat();                        // insertの処理
+      await createExpense();                        // insertの処理
     }
 
     Navigator.of(context).pop();                // 前の画面に戻る
   }
 
   // 更新処理の呼び出し
-  Future updateCat() async {
-    final cat = widget.cats!.copy(              // 画面の内容をcatにセット
-      name: name,
-      birthday: birthday,
-      gender: gender,
-      memo: memo,
+  Future updateExpense() async {
+    final expense = widget.expenses!.copy(              // 画面の内容をcatにセット
+      expense_id: expense_id,
+      expense_total_money: expense_total_money,
+      expense_consuption_tax: expense_consuption_tax,
+      expense_amount_including_tax: expense_amount_including_tax,
+      expense_datetime: expense_datetime,
+      expense_memo: expense_memo,
+      created_at: created_at,
     );
 
-    await DBHelper.instance.update(cat);        // catの内容で更新する
+    await DBHelper.instance.update(expense);        // catの内容で更新する
   }
 
   // 追加処理の呼び出し
-  Future createCat() async {
-    final cat = Expenses(                           // 入力された内容をcatにセット
-      name: name,
-      birthday: birthday,
-      gender: gender,
-      memo: memo,
-      createdAt: createdAt,
+  Future createExpense() async {
+    final expense = Expenses(                           // 入力された内容をcatにセット
+      expense_id: expense_id,
+      expense_total_money: expense_total_money,
+      expense_consuption_tax: expense_consuption_tax,
+      expense_amount_including_tax: expense_amount_including_tax,
+      expense_datetime: expense_datetime,
+      expense_memo: expense_memo,
+      created_at: created_at,
     );
-    await DBHelper.instance.insert(cat);        // catの内容で追加する
+    await DBHelper.instance.insert(expense);        // catの内容で追加する
   }
 }
